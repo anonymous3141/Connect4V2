Get backprop running in unit tests, fix random seed issue

models/Architectures.py
@@ -47,7 +47,7 @@ class Architecture1(nn.Module):
 
   def __init__(self):
     super(Architecture1, self).__init__() 
-    self.feature_stack = nn.Sequential(nn.Conv2d(2,NUM_NODES,4),
+    self.feature_stack = nn.Sequential(nn.Conv2d(2,NUM_NODES,4), #in channels, out channels, filter size
                                       nn.ReLU(inplace=True),
                                       nn.Flatten(start_dim=1, end_dim=-1))
     self.linear_stack = nn.Sequential(nn.Linear(NUM_NODES * 3 * 4,NUM_NODES),

models/MCTS.py
@@ -30,10 +30,11 @@ class Node:
         # epsilon greedy
         # implicitly contains randomised rollout policy upon reaching 
         # leaf of the expanded tree
-        if np.random.uniform() < EPS_MCTS:
+        if np.random.random()  < EPS_MCTS:
             # np.random_choice not always trusted
             # e.g 1 and 6 are never picked if seed = 0
             self.ptr = (self.ptr + 1) % len(self.validMoves)
+            #print(self.ptr)
             return self.validMoves[self.ptr]
         
         bestMoves = [self.validMoves[0]]
@@ -68,8 +69,7 @@ class MCTSModel(IModel):
     rollout() -> execute rollout policy
     """
     def __init__(self):
-        np.random.seed(42)
-    
+        pass
     def rollout(self, gameState):
         # input: gameState is copy of cur game state
         # use randomised rollout to simulate game state

models/NNModel.py
@@ -1,20 +1,61 @@
 import torch
-import torch.nn
+import torch.nn as nn
+from .IModel import IModel
+import numpy as np
+
+DISCOUNT_FACTOR = 0.9
+LEARNING_RATE = 0.0003
+LOSS_FN = nn.MSELoss()
 
 class NNModel(IModel):
     def __init__(self):
-        self.model = None
+        self.position_scorer = None
+        self.optimizer = None
         self.gameStates = []
 
-    def set_model(model):
-        self.model = model
+    def set_position_scorer(self, model):
+        self.position_scorer = model.double()
+        self.optimizer = torch.optim.Adam(\
+            self.position_scorer.parameters(),\
+            lr = LEARNING_RATE)
 
     def move(self, gameState):
-        pass
+        # input: duplicated gameState
+        with torch.inference_mode():
+            best_move = -1
+            best_move_score = -100000
+            turn_sign = 1 if gameState.getTurn() == 1 else -1 # p1 maximise, p2 minimise
+            for move in gameState.getValidMoves():
+                newState = gameState.duplicate()
+                newState.play(move)
+                move_score = self.position_scorer(torch.tensor(newState.toNPArray()).unsqueeze(0).double())
+                if turn_sign * move_score > turn_sign * best_move_score: # minimax
+                    best_move = move 
+                    best_move_score = move_score 
+            
+            gameState.play(best_move)
+            self.gameStates.append(gameState)
+            return best_move 
+            
     
     # apply backprop to list of cached data
     def gameOver(self, reward):
-        pass
+        # apply exponentially weighted final reward as a ground truth
+        # single batch
+        training_input = np.zeros((len(self.gameStates), 2, 6, 7))
+        ground_truth = [reward]
 
+        for i in range(len(self.gameStates)):
+            training_input[i,:] = self.gameStates[i].toNPArray() # np syntax nice
+        
+        for i in range(len(self.gameStates)-1):
+            ground_truth.append(ground_truth[-1]*DISCOUNT_FACTOR)
+        
+        outputs = self.position_scorer(torch.tensor(training_input).double()).squeeze(0)
+        loss = LOSS_FN(outputs, torch.tensor(ground_truth[::-1]).double()) # compute loss
+        loss.backward() # generate gradients
+        self.optimizer.step() # make a optimization step
+
+    # save neural net model
     def save(self, filename):
-        torch.save(self.model.state_dict(), filename)
+        torch.save(self.position_scorer.state_dict(), filename)

models/OneMoveLookAhead.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class OneMoveLookAhead(IModel):
     def __init__(self):
-        np.random.seed(42)
+        pass
     
     def move(self, gameState):
         # plays winning move if able

models/RandomPlayer.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class RandomPlayer(IModel):
     def __init__(self):
-        np.random.seed(42)
+        pass
     def move(self, gameState):
         assert(not gameState.isTerminal())
         return np.random.choice(gameState.getValidMoves()).item()

tests/testModels.py
@@ -9,20 +9,44 @@ from connect4 import ConnectFour
 from models.OneMoveLookAhead import OneMoveLookAhead
 from models.RandomPlayer import RandomPlayer
 from models.MCTS import MCTSModel
+from models.NNModel import NNModel 
+from models.Architectures import *
+import numpy as np
 
-def testModel(model, sequence, expectedMoves):
+def testModel(model, sequence, expectedMoves, nn=None):
+    print("------------")
     env = ConnectFour()
     for i in range(len(sequence)):
         env.play(sequence[i])
+    
+    if nn != None:
+        # duck typing OP
+        model.set_position_scorer(nn)
+
+    # test if next move is expected
     res = model.move(env)
     if res not in expectedMoves:
         print(f"Wrong Answer: Expected something in {expectedMoves}, got {res}")
     else:
         print(f'Accepted, received {res}')
 
+    if nn != None:
+        # test backward, prematurely end
+        model.gameOver(env.getResult())
+        print('Backprop Ran Successfully')
+        
+np.random.seed(42)
+# test OneMoveLookAhead
 testModel(OneMoveLookAhead(), [0,1,0,1,0,1], [0])
 testModel(OneMoveLookAhead(), [0,1,0,1,0,1,2],[1])
+
+# test MCTS
+# just gotta accept some WA np rng is kinda shitty
 testModel(MCTSModel(), [0,1,0,1,0,1], [0])
 testModel(MCTSModel(), [0,1,0,1,0,1,2],[1])
 testModel(MCTSModel(), [0,1,1,2,2,3,2,3,3,0], [3])
 testModel(MCTSModel(), [3,3,2,3,4,3], [1,5])
+
+# test NN
+testModel(NNModel(), [3,3,2,3,4,3], [0,1,2,3,4,5,6], Architecture1()) # test nn not crash on train
+testModel(NNModel(), [0,1,0,1,0,1], [0,1,2,3,4,5,6], Architecture1()) # test nn not crash on backward